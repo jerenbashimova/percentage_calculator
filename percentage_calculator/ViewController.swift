6201c11048fe8ba55c44d986ef0a4372635cf0e2
jerenbashimova <129326466+jerenbashimova@users.noreply.github.com>
2023-07-24 12:28:53 +0300

    Initial Commit

---
+++ percentage_calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  percentage_calculator
+//
+//  Created by j.bashimova on 24.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
